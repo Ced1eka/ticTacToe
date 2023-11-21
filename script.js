7b8860d0d7f081efe314adb0c07dd924b4c85b78
Ced <cedieka2@gmail.com>
2023-11-22 04:36:30 +0900

    Made a get board function.
    It looks fine for now?

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log ('hello test!')
+(function(){
+
+let board = {
+
+      players: {
+            // player1,player2
+      },
+
+      getboard: function(){
+            board = [];
+            const rows = 3;
+            const columns =3;
+
+            for(let i = 0; i < rows; i++){
+                  board.push([]);
+            for(let j = 0; j < columns; j++){
+                        board[i].push('something');
+            }
+            }
+            return board;
+      },
+}
+console.log(board.getboard());
+})()
