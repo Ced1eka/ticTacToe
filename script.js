37a9b0e9518f084145eda916a162b8d0013329b0
Ced <cedieka2@gmail.com>
2023-11-26 04:54:45 +0900

    Make token add func.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const ticTac = (function () {
     },
   ];
 
-  //make a 2D array of the board array
+  //make a 2D array of the board array, that shows it
   const getboard = function () {
     const boardSize = 3;
     let cellNumber = 1; //cell number counter
@@ -27,15 +27,19 @@ const ticTac = (function () {
         cell.push(cellNumber); // Assign cell number
         cellNumber++; // Increment cell number counter
 
-        board[i].push(cell); // Add the cell to the board array
-
+        board[i].push('-'); // Add the cell to the board array, put player input here later
+        console.log(`this is cell no:${cell} on board ${board[i][j]}`)
         // board[i].push("-");
       }
+
     }
     return board;
   };
 
   const gameController = function () {
+
+    const showBoard = getboard();
+
     // switches current player turn, this immediately fires so fix this in a bit
     let currentPlayer = player[0];
     const playerTurnSwitch = (function () {
@@ -44,42 +48,49 @@ const ticTac = (function () {
 
     //shows who's turn it is currently
     const getCurrentPlayer = () => currentPlayer;
-    console.log(getCurrentPlayer());
+    // console.log(getCurrentPlayer());
 
     // next i need a way to basically loop through the board during each player's turn
     //to see what row and column they choose
+
+    //check if a cell on board is empty, fix this later?
     const isEmpty = (row, column) => {
-      return this.board[row][column] === "something";
-    };
-
-    const playerMove = (currentPlayer, row, column) => {
-      if (!isEmpty(row, column)) {
-        console.log("occupado!");
+      if(showBoard[row][column] === "-"){
+        console.log('ye?')
         return;
+      }else {
+        playerMove();
       }
-      this.board[row][column] = player;
     };
 
-    const dispBoard = () => {
-      console.clear();
-      for (let i = 0; i < 3; i++) {
-        console.log(this.board[i].join("|"));
-      }
+    //get player token and place value inside given cell(temporarily in console)
+    const playerMove = (currentPlayer, row, column) => {
+      // if (!isEmpty(row, column)) {
+      //   console.log("occupado!");
+      //   return;
+      // }
+
+      const currentPlayerToken = currentPlayer['token'];
+      showBoard[row][column] = currentPlayerToken;
     };
+
+
+
+    // Prompt the user for the row and column temp
+    const row = prompt("Enter the row (0, 1, or 2): ");
+    const column = prompt("Enter the column (0, 1, or 2): ");
+
+    // Place the player's token in the selected cell
+    playerMove(currentPlayer, row, column);
+
+    return {playerTurnSwitch,getCurrentPlayer, currentPlayer, playerMove};
   };
   return { getboard, gameController };
 })();
 
 /*
-create an array to store the 3X3 grid board
-
-create an array to store the player objects
-
-create function to render the board grid
-
-create function to keep track of player turn
-
 create funtion to let players put marks on certain parts of board
 
 create function to make it game over when either the board is full or player has 3 marks in line
+
 */
